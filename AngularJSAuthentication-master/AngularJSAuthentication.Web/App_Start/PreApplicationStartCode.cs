31388923f3aa5351550feb6ee79732b17548d1fb
jdoan3 <jdoan9@gmail.com>
2017-06-08 15:17:46 -0500

    Update with Prerender in Web Project

---
+++ AngularJSAuthentication-master/AngularJSAuthentication.Web/App_Start/PreApplicationStartCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+namespace AngularJSAuthentication.Web.App_Start
+{
+    public static class PreApplicationStartCode
+    {
+        private static bool _isStarting;
+
+        public static void PreStart()
+        {
+            if (!_isStarting)
+            {
+                _isStarting = true;
+
+                DynamicModuleUtility.RegisterModule(typeof(Prerender.io.PrerenderModule));
+            }
+        }
+    }
+}
